2696ec7b12bbb4c9488792eb3810ff607a22c026
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2020-06-08 14:28:55 +0200

    changed app routing

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService, AppDetails } from './app.service';
 
-@Controller()
+@Controller('app')
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
+  @Get('details')
   details(): AppDetails {
     return this.appService.getDetails();
   }
