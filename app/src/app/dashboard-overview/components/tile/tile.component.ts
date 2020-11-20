8f93922f9effa33e65a48783e03c99375f3aba19
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2020-11-20 14:09:00 +0100

    add tiles #39

--- app/src/app/dashboard-overview/components/tile/tile.component.ts
+++ app/src/app/dashboard-overview/components/tile/tile.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tile',
   templateUrl: './tile.component.html',
   styleUrls: ['./tile.component.css']
 })
-export class TileComponent implements OnInit {
+export class TileComponent implements OnInit, AfterContentChecked {
+
+  hasFooter: boolean;
+  hasHeader: boolean;
+
+  @ViewChild('header')
+  private header!: ElementRef<HTMLElement>;
+
+  @ViewChild('footer')
+  private footer!: ElementRef<HTMLElement>;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterContentChecked() {
+    this.hasHeader = this.header?.nativeElement.childElementCount > 0;
+    this.hasFooter = this.footer?.nativeElement.childElementCount > 0;
   }
+
+  ngOnInit() { }
 
 }
