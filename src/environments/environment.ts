443fa0d9b72d6f43a02f234184bf06fc2209d629
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-04-12 21:51:29 +0200

    #1 lint fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDky1CHqZKZ-GyE6O3RNXILNydBZIBVAHI",
-    authDomain: "beez-6f2c5.firebaseapp.com",
-    projectId: "beez-6f2c5",
-    storageBucket: "beez-6f2c5.appspot.com",
-    messagingSenderId: "280318214840",
-    appId: "1:280318214840:web:c054a3e372247afc432a4d",
-    measurementId: "G-XDN9847E53"
+    apiKey: 'AIzaSyDky1CHqZKZ-GyE6O3RNXILNydBZIBVAHI',
+    authDomain: 'beez-6f2c5.firebaseapp.com',
+    projectId: 'beez-6f2c5',
+    storageBucket: 'beez-6f2c5.appspot.com',
+    messagingSenderId: '280318214840',
+    appId: '1:280318214840:web:c054a3e372247afc432a4d',
+    measurementId: 'G-XDN9847E53'
   }
 };
 
