9544a9ba2cb1e3d28a99f00315f9601cc069d4e2
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-04-13 22:12:22 +0200

    #2 working tests

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,12 +1,26 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Subject } from 'rxjs';
 
 import { AuthService } from './auth.service';
+import { User } from './user';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: AngularFireAuth,
+          useValue: {
+            authState: new Subject<User>(),
+            signInWithRedirect: jest.fn(),
+            signOut: jest.fn()
+          }
+        }
+      ]
+    });
     service = TestBed.inject(AuthService);
   });
 
