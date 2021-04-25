3d3b54579da557fa16d029611675535f29f074af
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-04-25 21:00:16 +0200

    #7 sort table

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
-import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
@@ -24,6 +25,7 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatInputModule,
     MatListModule,
     MatSidenavModule,
+    MatSortModule,
     MatTableModule,
     MatToolbarModule,
     LayoutModule
@@ -37,6 +39,7 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatInputModule,
     MatListModule,
     MatSidenavModule,
+    MatSortModule,
     MatToolbarModule,
     MatTableModule,
     LayoutModule
