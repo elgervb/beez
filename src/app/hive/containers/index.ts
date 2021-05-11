f506ca726cf23cd7c725c0d553266094b8e3f669
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-05-11 22:18:32 +0200

    #31 fix lint

--- src/app/hive/containers/index.ts
+++ src/app/hive/containers/index.ts
@@ -1,3 +1,3 @@
 export * from './hive-list/hive-list.component';
 export * from './hive-edit/hive-edit.component';
-export * from './hive-details/hive-details.component'
+export * from './hive-details/hive-details.component';
