d949b8cec52eb5e7b3a8ceedd886aeed382b090c
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-06-08 23:05:10 +0200

    #54 print qrcode

---
+++ src/app/hive/components/qr-dialog/qr-dialog.component.spec.ts
@@ -0,0 +1,42 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels, NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+import { QRDialog, QrDialogComponent } from './qr-dialog.component';
+
+describe('QrDialogComponent', () => {
+  let component: QrDialogComponent;
+  let fixture: ComponentFixture<QrDialogComponent>;
+  const data: QRDialog = {
+    correctionLevel: NgxQrcodeErrorCorrectionLevels.HIGH,
+    elementType: NgxQrcodeElementTypes.URL,
+    qrValue: 'my_string'
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [QrDialogComponent],
+      imports: [
+        MaterialModule,
+        NgxQRCodeModule,
+        SharedModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: data }
+      ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QrDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/hive/components/qr-dialog/qr-dialog.component.ts
@@ -0,0 +1,26 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
+
+export interface QRDialog {
+  elementType: NgxQrcodeElementTypes;
+  correctionLevel: NgxQrcodeErrorCorrectionLevels;
+  qrValue: string;
+}
+
+@Component({
+  selector: 'bee-qr-dialog',
+  templateUrl: './qr-dialog.component.html',
+  styleUrls: ['./qr-dialog.component.css']
+})
+export class QrDialogComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: QRDialog) { }
+
+  ngOnInit(): void {
+  }
+
+  print(): void {
+    window.print();
+  }
+}
