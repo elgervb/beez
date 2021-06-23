ea951bd2b2e17bb2c60b581a4670c7a72029f26d
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-06-23 10:54:12 +0200

    #61 fix health indicator

--- src/app/inspection/containers/inspection-list/inspection-list.component.spec.ts
+++ src/app/inspection/containers/inspection-list/inspection-list.component.spec.ts
@@ -1,7 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { transform } from '@elgervb/mock-data';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { Inspection } from '../../models';
 import { InspectionService } from '../../services/inspection.service';
 
 import { InspectionListComponent } from './inspection-list.component';
@@ -32,5 +34,40 @@ describe('InspectionListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('trendingIconName', () => {
+
+    it('should go up', () => {
+      const inspections = [
+        transform<Inspection>({ health: 20 }),
+        transform<Inspection>({ health: 10 }),
+      ];
+
+      expect(component.trendingIconName(inspections[0], inspections)).toBe('trending_up');
+    });
+
+    it('should go down', () => {
+      const inspections = [
+        transform<Inspection>({ health: 10 }),
+        transform<Inspection>({ health: 20 }),
+      ];
+
+      expect(component.trendingIconName(inspections[0], inspections)).toBe('trending_down');
+    });
+
+    it('should stay the same', () => {
+      const inspections = [
+        transform<Inspection>({ health: 10 }),
+        transform<Inspection>({ health: 10 }),
+      ];
+
+      // no list to compare to
+      expect(component.trendingIconName(inspections[0])).toBe('compare_arrows');
+      // not in the list
+      expect(component.trendingIconName(transform<Inspection>({ health: 10 }), inspections)).toBe('compare_arrows');
+      // stay the same
+      expect(component.trendingIconName(inspections[0], inspections)).toBe('compare_arrows');
+    });
   });
 });
