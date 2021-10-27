4f6bdf408496fd1a3f386c69935f7755b5d6ca0b
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-10-27 13:15:42 +0200

    #81 fix bug print qrcode

--- src/app/hive/components/qr-dialog/qr-dialog.component.ts
+++ src/app/hive/components/qr-dialog/qr-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
 
@@ -8,14 +8,27 @@ export interface QRDialog {
   qrValue: string;
 }
 
+export const cssClass = 'qr-print-class';
+
 @Component({
   selector: 'bee-qr-dialog',
   templateUrl: './qr-dialog.component.html',
   styleUrls: [ './qr-dialog.component.css' ]
 })
-export class QrDialogComponent {
+export class QrDialogComponent implements OnInit, OnDestroy {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: QRDialog) { }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: QRDialog,
+    private renderer: Renderer2
+  ) { }
+
+  ngOnInit(): void {
+    this.renderer.addClass(document.body, cssClass);
+  }
+
+  ngOnDestroy(): void {
+    this.renderer.removeClass(document.body, cssClass);
+  }
 
   print(): void {
     window.print();
