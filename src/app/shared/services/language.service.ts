518eaf16f66891ff268316b9a56785d9a1c8072f
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-10-27 12:26:10 +0200

    #63 remove dynamic import statement as it results in runtime errors

--- src/app/shared/services/language.service.ts
+++ src/app/shared/services/language.service.ts
@@ -1,6 +1,4 @@
-import { registerLocaleData } from '@angular/common';
 import { Injectable } from '@angular/core';
-// import { registerLocaleData } from '@angular/common';
 import { I18NextService } from 'angular-i18next';
 import { from, Observable, of } from 'rxjs';
 
@@ -20,21 +18,11 @@ export class LanguageService {
       this.i18NextService.changeLanguage(lang);
       // eslint-disable-next-line @typescript-eslint/no-empty-function
       const promise = this.i18NextService.loadLanguages(lang, () => {})
-        .then(() => this.localeInitializer(lang))
       // eslint-disable-next-line @typescript-eslint/no-empty-function
         .then(() => {});
       return from(promise);
     }
     return of(void 0);
-  }
-
-  /** https://medium.com/angular-in-depth/dynamic-import-of-locales-in-angular-b994d3c07197 */
-  private localeInitializer(localeId: string): Promise<unknown> {
-    // eslint-disable-next-line function-paren-newline
-    return import(
-      /* webpackInclude: /(nb|sv)\.js$/ */
-      `@angular/common/locales/${localeId}`)
-      .then(module => registerLocaleData(module.default));
   }
 
 }
