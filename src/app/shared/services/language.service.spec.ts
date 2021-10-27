7ea97fc6c22e0a3c7dd333dd4b7fea993a2ba3ee
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-10-27 08:51:05 +0200

    #63 all tests go green

--- src/app/shared/services/language.service.spec.ts
+++ src/app/shared/services/language.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { I18nextTestingModule } from '../testing';
 
 import { LanguageService } from './language.service';
 
@@ -6,7 +7,9 @@ describe('LanguageService', () => {
   let service: LanguageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ I18nextTestingModule ]
+    });
     service = TestBed.inject(LanguageService);
   });
 
--- src/app/shared/services/preferences.service.spec.ts
+++ src/app/shared/services/preferences.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from 'auth';
 
 import { PreferencesService } from './preferences.service';
 
 describe('PreferencesService', () => {
+
+  const authService = jest.fn();
+  const angularFirestore = jest.fn();
   let service: PreferencesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: AuthService, useValue: authService },
+        { provide: AngularFirestore, useValue: angularFirestore },
+      ]
+    });
     service = TestBed.inject(PreferencesService);
   });
 
