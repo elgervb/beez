5df25eb1414896346d01d0fa625d2219aa7d0602
Elger van Boxtel <elger.vanboxtel@malmberg.nl>
2021-12-28 14:14:03 +0100

    #102 fix test setup

--- projects/micro-app/src/setup-jest.ts
+++ projects/micro-app/src/setup-jest.ts
@@ -1,9 +1,11 @@
-import { randomNumber, randomString, register } from '@elgervb/mock-data';
+import { randomDate, randomNumber, randomString, register } from '@elgervb/mock-data';
 import { LedgerEntry, LedgerEntryType } from './lib/ledger/models';
+
+import firebase from 'firebase';
 
 register<LedgerEntry>('LedgerEntry', {
   amount: () => randomNumber(1, 100, 2),
-  date: () => new Date(),
+  date: () => new firebase.firestore.Timestamp(randomDate().getTime() / 1000, 0),
   description: () => randomString(10),
   type: () => randomNumber(1, 10) % 2 ? LedgerEntryType.income : LedgerEntryType.expense
 });
