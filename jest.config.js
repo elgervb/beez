52bac2d4d1e1c80c3199e7c98209830c06fa13db
Elger van Boxtel <elgervb@users.noreply.github.com>
2023-03-23 16:59:31 +0100

    test with jest (#114)

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  displayName: 'BEEZ',
+  coverageThreshold: {
+    global: {
+      statements: 75,
+      branches: 75,
+      functions: 75,
+      lines: 75
+    }
+  },
+};
